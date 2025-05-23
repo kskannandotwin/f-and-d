8ca10b964fda107d4cfd056a9154611cbcec8512
kannanATexcelforte <kannan@excelforte.com>
2025-05-23 07:20:06 +0530

    23.05.2025
    conflict verified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { DirectivesComponent } from './directives/directives.component';
 import { UserComponent } from './user/user.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { StructuralDirectiveNgifVsIfComponent } from './structural-directive-ngif-vs-if/structural-directive-ngif-vs-if.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DataBindingComponent],
+  imports: [RouterOutlet, StructuralDirectiveNgifVsIfComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
