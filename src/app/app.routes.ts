776cbceb20b0d7dbf9ac18ea6e3708eddb859190
kannanATexcelforte <kannan@excelforte.com>
2025-05-24 17:24:14 +0530

    24.05.2025
    completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
-import { StructuralDirectiveNgforVsForComponent } from './structural-directive-ngfor-vs-for/structural-directive-ngfor-vs-for.component';
 import { AttributeDirectivesComponent } from './attribute-directives/attribute-directives.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { StructuralDirectiveNgifVsIfComponent } from './structural-directive-ngif-vs-if/structural-directive-ngif-vs-if.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'user', pathMatch: 'full' },
     { path: 'user', component: UserComponent },
     { path: 'data-binding', component: DataBindingComponent },
-    { path: 'structural-directive', component: StructuralDirectiveNgforVsForComponent },
+    { path: 'structural-directive', component: StructuralDirectiveNgifVsIfComponent },
     { path: 'attribute-directive', component: AttributeDirectivesComponent },
     { path: '**', component: PageNotFoundComponent } // Wildcard route for a 404 page or redirect
 ];
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,13 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user',
-  imports: [CommonModule, FormsModule],
+  standalone: true,
+  imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
 
+  constructor(private router: Router) { }
+  login() {
+    // user verification logic
+    // this.router.navigateByUrl('structural-directive');
+    this.router.navigate(['structural-directive']);
+  }
 }
