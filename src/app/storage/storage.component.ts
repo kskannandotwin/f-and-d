9482c298961ef7aed93b3eac7709e986ec07dc1c
kannanATexcelforte <kannan@excelforte.com>
2025-06-03 08:08:36 +0530

    03.06.2025
    completed

--- src/app/storage/storage.component.ts
+++ src/app/storage/storage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-storage',
@@ -8,8 +9,11 @@ import { Component } from '@angular/core';
 })
 export class StorageComponent {
 
+  constructor(private cookie: CookieService) {}
+
   sessionValue: string = '';
   localValue: string = '';
+  cookieValue: string = '';
   setSession() {
     sessionStorage.setItem('name', 'session');
     sessionStorage.setItem('pass', 'session123');
@@ -44,5 +48,20 @@ export class StorageComponent {
   clearLocal() {
     localStorage.clear();
     this.localValue = '';
+  }
+
+  setCookie() {
+    this.cookie.set('token1', '12345', 1);
+    this.cookie.set('token2', 'xyz');
+  }
+
+  getCookie() {
+    this.cookieValue = this.cookie.get('token1');
+  }
+
+  deleteCookie() {
+    // this.cookie.delete('token1');
+    this.cookie.deleteAll(); // Deletes all cookies
+    this.cookieValue = '';
   }
 }
