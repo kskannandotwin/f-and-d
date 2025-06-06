9c35211a8c5e1579d44c3f3273e2b892322b6873
kannanATexcelforte <kannan@excelforte.com>
2025-06-06 06:44:39 +0530

    06.06.2025
    interpolation, property binding, event binding and two way binding templates

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -9,16 +8,52 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './data-binding.component.scss'
 })
 export class DataBindingComponent {
-  name: string = 'FED Learning';
-  topic: string = 'Data Binding';
-  image: string = 'https://angular.io/assets/images/logos/angular/angular.png';
-  random: string = '';
+  name: string = 'GitHub Copilot';
+  age: number = 5;
+  imageUrl: string = 'https://angular.io/assets/images/logos/angular/angular.png';
+  isDisabled: boolean = false;
+  buttonClass: string = 'btn-primary';
+  titleColor: string = 'blue';
 
-  onSave() {
-    alert('Data saved successfully!');
+  clickCount: number = 0;
+  inputText: string = '';
+
+  username: string = '';
+  email: string = '';
+  message: string = '';
+
+  getAgePlusFive(): number {
+    return this.age + 5;
   }
 
-  onChange() {
-    alert('Country has changed!');
+  onClick() {
+    this.clickCount++;
+    console.log('Button clicked!');
+  }
+
+  onMouseOver() {
+    console.log('Mouse over event triggered');
+  }
+
+  onInputChange(event: any) {
+    this.inputText = event.target.value;
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    
+    // Get all form values
+    const formData = {
+      username: this.username,
+      email: this.email,
+      message: this.message
+    };
+    
+    console.log('Form Data:', formData);
+    
+    // Reset form fields after submission
+    this.username = '';
+    this.email = '';
+    this.message = '';
   }
 }
